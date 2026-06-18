93e56db8b9d984629fc1fb720a074260795dd98e
Uyen Doan <smmr-dn@users.noreply.github.com>
2026-06-18 00:09:59 -0500

    styled header

--- client/src/components/Venue.jsx
+++ client/src/components/Venue.jsx
@@ -29,15 +29,9 @@ const Venue = () => {
       style={{ boxShadow: "0px 0px 0px 0px" }}
     >
       <div className="venue-title">
-        <div className="venue-pin-drop" style={{ background: venue.color }}>
-          <i
-            className={`ti ti-${venue.icon}`}
-            style={{ color: venue.textColor }}
-          />
-        </div>
         <h3>{venue.name}</h3>
+        <p>{venue.tagline}</p>
       </div>
-      <p>{venue.tagline}</p>
 
       <div className="venue-events">
         {eventsByVenueId.map((event) => (
